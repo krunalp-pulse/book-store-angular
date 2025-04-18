de07a77a42db728fac99871eccf9912ee4945721
krunalp-pulse <krunal.parmar@PS97.Team>
2025-04-18 15:27:31 +0530

    Get form control reference

--- bookStoreApp/src/app/books/components/add-book-reactive/add-book-reactive.component.ts
+++ bookStoreApp/src/app/books/components/add-book-reactive/add-book-reactive.component.ts
@@ -24,6 +24,9 @@ export class AddBookReactiveComponent implements OnInit {
 
   ngOnInit(): void {
     this.intiForm();
+
+    console.log(this.addBookForm.controls.title);
+    console.log(this.addBookForm.get('title'));
   }
   updateFormValues(): void {
     this.addBookForm.patchValue({
